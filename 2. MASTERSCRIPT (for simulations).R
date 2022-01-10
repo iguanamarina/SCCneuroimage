77e55f535d4417a530c934f1a9f69b29be0bf303
Juan A. Arias <34753691+iguanamarina@users.noreply.github.com>
2022-01-10 13:23:44 +0100

    Updates on Project

--- 2. MASTERSCRIPT (for simulations).R
+++ 2. MASTERSCRIPT (for simulations).R
@@ -857,10 +857,11 @@ graph2
 graph2 <- ggplot(data = table, 
                  aes(x = Roi, y = sens)) + 
                  geom_boxplot(outlier.colour = NULL, aes(fill = method), outlier.size = 1) +
-                 xlab("Level of Induced Hypoactivity (%)") + 
-                 ylab("Sensibility (%)") + ggtitle(paste0("Comparison of sensibility by regions in z=", as.numeric(param.z))) +
+                 xlab("Induced Hypoactivity (%)") + 
+                 ylab("Sensibility (%)") + 
+                 # ggtitle(paste0("Comparison of sensibility by regions in z=", as.numeric(param.z))) +
                  guides(fill = guide_legend(title = "Methods")) + 
-                 facet_wrap( ~ region) 
+                 facet_wrap( ~ region, nrow = 3) 
 
 graph2 
 
@@ -868,10 +869,11 @@ graph2
 graph22 <- ggplot(data = table, 
                  aes(x = Roi, y = esp)) + 
                  geom_boxplot(outlier.colour = NULL, aes(fill = method), outlier.size = 1) +
-                 xlab("Level of Induced Hypoactivity (%)") + 
-                 ylab("Especificity (%)") + ggtitle(paste0("Comparison of especificity by regions in z=", as.numeric(param.z))) +
+                 xlab("Induced Hypoactivity (%)") + 
+                 ylab("Specificity (%)") + 
+                 # ggtitle(paste0("Comparison of especificity by regions in z=", as.numeric(param.z))) +
                  guides(fill = guide_legend(title = "Methods")) + 
-                 facet_wrap( ~ region) 
+                 facet_wrap( ~ region, nrow = 3) 
 
 graph22 
 
@@ -883,12 +885,53 @@ graph22
 
 install.packages("envalysis")
 library(envalysis)
-graph2 + theme_publish() 
+graph22 + theme_publish() 
 
 ggsave(
   "academic_plot.png",
   plot = last_plot(),
   scale = 2)
 
+ggsave(filename = "academic_plot.png", 
+       plot = last_plot(), 
+       width = 210, 
+       height = 297, 
+       units = "mm")
 
 
+
+# latex Table:
+
+library(tidyverse)
+
+referencia[referencia$region == "w32", ]$region <- as.character("ROI1")
+
+referencia[referencia$region == "w79", ]$region <- as.character("ROI2")
+
+referencia[referencia$region == "w214", ]$region <- as.character("ROI3")
+
+referencia[referencia$region == "w271", ]$region <- as.character("ROI4")
+
+referencia[referencia$region == "w413", ]$region <- as.character("ROI5")
+
+referencia[referencia$region == "wroiAD", ]$region <- as.character("ROI6")
+
+referencia$roi <- referencia$roi*10
+referencia$roi <- as.character(referencia$roi)
+
+for (i in 1:nrow(referencia)) {
+referencia$sens[i] <- paste0(round(as.numeric(referencia$sensMEAN[i]), 2), as.character("±"), round(as.numeric(referencia$sensSD[i]),2)) 
+}
+
+for (i in 1:nrow(referencia)) {
+referencia$esp[i] <- paste0(round(as.numeric(referencia$espMEAN[i]), 2), as.character("±"), round(as.numeric(referencia$espSD[i]),2)) 
+}
+
+
+my_data <- as_tibble(referencia)[ , c(1, 2, 3, 8, 9)]
+
+my_data <- my_data %>% arrange(method, region)
+
+print(xtable(my_data, type = "latex"), file = "my_table.tex")
+
+write.table(my_data, "ourTable.txt", quote=FALSE, eol="\\\\\n", sep=" & ")
