4b4db7ab854f16e72e722370d2b18ce3c78e10e9
Juan A. Arias <34753691+iguanamarina@users.noreply.github.com>
2022-01-13 14:24:52 +0100

    Update 3. SCRIPT FOR GRAPHS AND VISUALIZATION.R

--- 3. SCRIPT FOR GRAPHS AND VISUALIZATION.R
+++ 3. SCRIPT FOR GRAPHS AND VISUALIZATION.R
@@ -1,53 +1,96 @@
+############################## ################### ############## ### 
+##
+## Script name: SCRIPT FOR GRAPHS AND VISUALIZATION
+##
+## Purpose of script: Visualize data obtained from MASTERSCRIPT of the
+## SCC Neuroimage project.
+##
+## Date Created: 2022-01-13
+##
+## Author: Juan A. Arias (M.Sc.)
+## Email: juanantonio.arias.lopez@usc.es
+## Webpage: https://messy-dataset.xyz
+##
+## Notes:
+##       
+##   
+############################## ################### ############## ### 
 
+####  
+# PREAMBLE: ----
+#### 
 
+#* Set working directory: ----
 
+setwd("~/GitHub/SCCneuroimage") 
 
+#* Tune Options: ----
+options(scipen = 6, digits = 4) # View outputs in non-scientific notation
+memory.limit(30000000)     # This is needed on some PCs to increase memory allowance
 
-### ########################################################## ###
-#####               SCRIPT  FOR  VISUALIZATION                ####
-### ########################################################## ###
+#* Load up packages: ---- 
 
-require(ggplot2)
+# install.packages("envalysis")
+library(envalysis); library(tidyverse)
+require(ggplot2); require(gridExtra)
+
+#* Hyperparameter: ----
+
 param.z = 30
-setwd(paste0("C:/Users/Juan A. Arias/Desktop/Simulaciones PET CHUS/Preliminary results/z", as.numeric(param.z)))
-referencia <- readRDS(paste0("C:/Users/Juan A. Arias/Desktop/Simulaciones PET CHUS/Preliminary results/z", as.numeric(param.z),"/SCC_vs_SPM.RDS"))
-SCC_vs_SPM <- readRDS(paste0("C:/Users/Juan A. Arias/Desktop/Simulaciones PET CHUS/Preliminary results/z", as.numeric(param.z),"/SCC_vs_SPM_complete.RDS"))
-table <- SCC_vs_SPM # For commodity
+
+####  
+# PART 1: Load Data ----------
+####  
+
+referencia <- readRDS(paste0("~/GitHub/SCCneuroimage/z", as.numeric(param.z),"/results/SCC_vs_SPM.RDS"))
+table <- readRDS(paste0("~/GitHub/SCCneuroimage/z", as.numeric(param.z),"/results/SCC_vs_SPM_complete.RDS"))
+  
+  
+####  
+# PART 2: Modify Data Structure ----------
+####  
+
 table$region <- factor(table$region,      
                        levels = c("w32", "w79", "w214", "w271", "w413", "wroiAD"))
 table$Roi <- as.numeric(table$Roi)
 table$Roi <- table$Roi*10
 table$Roi <- as.character(table$Roi)
 attach(table)
-View(table)
+View(table) # Quite beautiful
 
 
-########################################################################################
-########################################################################################
+####  
+# PART 3: Graphs and Plots ----------
+####  
 
-# Graph1: Sensibility for Only wroi for zoom analysis:
+
+#* Graph1 & Graph 2: Sens & Esp for wROI ----
 
 graph1 <- ggplot(data = table[table$region == "wroiAD", ], 
           aes(x = Roi, y = sens)) + 
+          coord_cartesian(ylim = c(0, 100)) +
           geom_boxplot(aes(fill = method)) + 
           xlab("Level of Induced Hypoactivity") + 
           ylab("Sensibility") + ggtitle(paste0("Comparison of SCC's and SPM's levels of sensibility for wroiAD in z=", as.numeric(param.z))) +
           guides(fill = guide_legend(title = "Legend"))
 
-graph1
-
 graph2 <- ggplot(data = table[table$region == "wroiAD", ], 
           aes(x = Roi, y = esp)) + 
           geom_boxplot(aes(fill = method)) + 
-          # coord_cartesian(ylim = c(0, 100)) +
+          coord_cartesian(ylim = c(0, 100)) + # necessary to specify because in most of cases the scale ranges from 90% to 100% only
           xlab("Level of Induced Hypoactivity") + 
           ylab("Especificity") + ggtitle(paste0("Comparison of SCC's and SPM's levels of especificity for wroiAD in z=", as.numeric(param.z))) +
           guides(fill = guide_legend(title = "Legend"))
 
-graph2
+plot1 <- graph1 + theme_publish(); plot2 <- graph2 + theme_publish()
+grid.arrange(plot1, plot2, ncol = 2)
 
-########################################################################################
-########################################################################################
+ggsave(filename = "academic_plot.png", 
+       plot = last_plot(), 
+       width = 210, 
+       height = 297, 
+       units = "mm")
+
 
 # Graph2: Sensibility for All regions & All ROIs
 
@@ -75,60 +118,3 @@ graph22 <- ggplot(data = table,
 graph22 
 
 
-########################################################################################
-########################################################################################
-
-# Academic Publication Theme:
-
-install.packages("envalysis")
-library(envalysis)
-graph22 + theme_publish() 
-
-ggsave(
-  "academic_plot.png",
-  plot = last_plot(),
-  scale = 2)
-
-ggsave(filename = "academic_plot.png", 
-       plot = last_plot(), 
-       width = 210, 
-       height = 297, 
-       units = "mm")
-
-
-
-# latex Table:
-
-library(tidyverse)
-
-referencia[referencia$region == "w32", ]$region <- as.character("ROI1")
-
-referencia[referencia$region == "w79", ]$region <- as.character("ROI2")
-
-referencia[referencia$region == "w214", ]$region <- as.character("ROI3")
-
-referencia[referencia$region == "w271", ]$region <- as.character("ROI4")
-
-referencia[referencia$region == "w413", ]$region <- as.character("ROI5")
-
-referencia[referencia$region == "wroiAD", ]$region <- as.character("ROI6")
-
-referencia$roi <- referencia$roi*10
-referencia$roi <- as.character(referencia$roi)
-
-for (i in 1:nrow(referencia)) {
-referencia$sens[i] <- paste0(round(as.numeric(referencia$sensMEAN[i]), 2), as.character("?"), round(as.numeric(referencia$sensSD[i]),2)) 
-}
-
-for (i in 1:nrow(referencia)) {
-referencia$esp[i] <- paste0(round(as.numeric(referencia$espMEAN[i]), 2), as.character("?"), round(as.numeric(referencia$espSD[i]),2)) 
-}
-
-
-my_data <- as_tibble(referencia)[ , c(1, 2, 3, 8, 9)]
-
-my_data <- my_data %>% arrange(method, region)
-
-print(xtable(my_data, type = "latex"), file = "my_table.tex")
-
-write.table(my_data, "ourTable.txt", quote=FALSE, eol="\\\\\n", sep=" & ")
